ecc1de0ad81d51090448632b8e9c02a767a2504f
K. Lange <klange@toaruos.org>
2021-01-15 07:28:45 +0900

    basic JS bindings

--- wasmmain.c
+++ wasmmain.c
@@ -2,6 +2,8 @@
  * Kuroko WASM REPL C entry point.
  */
 #include <stdint.h>
+#include <string.h>
+#include <emscripten.h>
 #include "kuroko.h"
 #include "vm.h"
 
@@ -17,6 +19,216 @@
 	krk_attachNamedObject(&AS_INSTANCE(moduleOut)->fields, "__name__", (KrkObj*)krk_copyString(#name, sizeof(#name)-1)); \
 	krk_attachNamedValue(&AS_INSTANCE(moduleOut)->fields, "__file__", NONE_VAL()); \
 } while (0)
+
+EM_JS(int32_t, krk_getKeyCount, (int i), {
+	console.log('Getting key count for object: ' + i);
+	if (typeof Module.krkb[i] === 'object' && Module.krkb[i] !== null)
+		return Object.keys(Module.krkb[i]).length;
+	return 0;
+});
+
+EM_JS(const char *, krk_getKey, (int obj, int key), {
+	var output = Object.keys(Module.krkb[obj])[key];
+	var bytes = lengthBytesUTF8(output)+1;
+	var heapObj = _malloc(bytes);
+	stringToUTF8(output, heapObj, bytes);
+	return heapObj;
+});
+
+EM_JS(const char *, krk_jsAsString, (int obj), {
+	var output = Module.krkb[obj];
+	var bytes = lengthBytesUTF8(output)+1;
+	var heapObj = _malloc(bytes);
+	stringToUTF8(output, heapObj, bytes);
+	return heapObj;
+});
+
+EM_JS(int32_t, krk_jsAsInt, (int obj), {
+	return Module.krkb[obj];
+});
+
+EM_JS(float, krk_jsAsFloat, (int obj), {
+	return Module.krkb[obj];
+});
+
+EM_JS(const char *, krk_jsErr, (), {
+	var output = Module.krkerr.name;
+	var bytes = lengthBytesUTF8(output)+1;
+	var heapObj = _malloc(bytes);
+	stringToUTF8(output, heapObj, bytes);
+	return heapObj;
+});
+
+EM_JS(int32_t, krk_jsType, (int i), {
+	if (Module.krkb[i] == null) return -1;
+	if (typeof Module.krkb[i] === 'string') return 1;
+	if (typeof Module.krkb[i] === 'boolean') return 2;
+	if (typeof Module.krkb[i] === 'number') return 3;
+	if (typeof Module.krkb[i] === 'function') return 4;
+	if (Array.isArray(Module.krkb[i])) return 5;
+	return 0;
+});
+
+static KrkClass * jsObjectClass;
+
+static KrkValue _jsobject_init(int argc, KrkValue argv[]) {
+	if (argc != 2) {
+		krk_runtimeError(vm.exceptions.argumentError, "Need a string argument of an object to build on");
+		return NONE_VAL();
+	}
+
+	if (!IS_STRING(argv[1])) {
+		krk_runtimeError(vm.exceptions.typeError, "Argument must be string");
+		return NONE_VAL();
+	}
+
+	KrkInstance * self = AS_INSTANCE(argv[0]);
+
+	char * tmp = malloc(40 + 2 * AS_STRING(argv[1])->length);
+	sprintf(tmp, "(typeof %s === 'undefined') ? -1 : Module.krkb.push(%s)-1", AS_CSTRING(argv[1]), AS_CSTRING(argv[1]));
+	int returnValue = emscripten_run_script_int(tmp);
+	free(tmp);
+
+	if (returnValue == -1) {
+		krk_runtimeError(vm.exceptions.nameError, "%s", AS_CSTRING(argv[1]));
+		return NONE_VAL();
+	}
+
+	int type = krk_jsType(returnValue);
+
+	switch (type) {
+		case -1: return NONE_VAL();
+		case 1: {
+			const char * bytes = krk_jsAsString(returnValue);
+			return OBJECT_VAL(krk_takeString((char*)bytes, strlen(bytes)));
+		}
+		case 2: return BOOLEAN_VAL(krk_jsAsInt(returnValue));
+		case 3: return FLOATING_VAL(krk_jsAsFloat(returnValue));
+	}
+
+
+	krk_attachNamedValue(&self->fields, "__jsname__", argv[1]);
+	self->_internal = AS_STRING(argv[1]);
+	krk_attachNamedValue(&self->fields, "__index__", INTEGER_VAL(returnValue));
+
+	return OBJECT_VAL(self);
+}
+
+static KrkValue _jsobject_dir(int argc, KrkValue argv[]) {
+	KrkInstance * self = AS_INSTANCE(argv[0]);
+	KrkValue outputList = krk_dirObject(argc,argv);
+	krk_push(outputList);
+	KrkValue _list_internal = OBJECT_VAL(AS_INSTANCE(outputList)->_internal);
+
+	KrkValue index;
+	krk_tableGet(&self->fields, OBJECT_VAL(krk_copyString("__index__",9)), &index);
+
+	int returnValue = AS_INTEGER(index);
+
+	int32_t keyCount = krk_getKeyCount(returnValue);
+	for (int32_t i = 0; i < keyCount; ++i) {
+		const char * val = krk_getKey(returnValue, i);
+		krk_writeValueArray(AS_LIST(_list_internal), OBJECT_VAL(krk_takeString((char*)val, strlen(val))));
+	}
+
+	krk_pop(); /* outputList */
+	return outputList;
+}
+
+static KrkValue _jsobject_getattr(int argc, KrkValue argv[]) {
+	if (argc != 2) {
+		krk_runtimeError(vm.exceptions.argumentError, "Need a string argument of an object to build on");
+		return NONE_VAL();
+	}
+
+	if (!IS_STRING(argv[1])) {
+		krk_runtimeError(vm.exceptions.typeError, "Argument must be string");
+		return NONE_VAL();
+	}
+
+	KrkInstance * self = AS_INSTANCE(argv[0]);
+	char * tmp = malloc(10 + AS_STRING(argv[1])->length + ((KrkString*)self->_internal)->length);
+	sprintf(tmp, "('%s' in %s)", AS_CSTRING(argv[1]), ((KrkString*)self->_internal)->chars);
+	int valid = emscripten_run_script_int(tmp);
+	if (!valid) {
+		free(tmp);
+		krk_runtimeError(vm.exceptions.attributeError, "%s", AS_CSTRING(argv[1]));
+		return NONE_VAL();
+	}
+
+	KrkInstance * newField = krk_newInstance(jsObjectClass);
+	krk_push(OBJECT_VAL(newField));
+
+	sprintf(tmp, "%s.%s", ((KrkString*)self->_internal)->chars, AS_CSTRING(argv[1]));
+	krk_push(OBJECT_VAL(krk_takeString(tmp, strlen(tmp))));
+
+	KrkValue actualResult = _jsobject_init(2, (KrkValue[]){krk_peek(1),krk_peek(0)});
+
+	krk_pop();
+	krk_pop();
+
+	if (IS_INSTANCE(actualResult)) {
+		krk_push(actualResult);
+		krk_tableSet(&self->fields, argv[1], krk_peek(0));
+		krk_pop();
+		return actualResult;
+	} else {
+		return actualResult;
+	}
+}
+
+static KrkValue _jsobject_call(int argc, KrkValue argv[]) {
+	KrkInstance * self = AS_INSTANCE(argv[0]);
+	KrkValue index;
+	krk_tableGet(&self->fields, OBJECT_VAL(krk_copyString("__index__",9)), &index);
+	KrkValue jsname;
+	krk_tableGet(&self->fields, OBJECT_VAL(krk_copyString("__jsname__",10)), &jsname);
+	int returnValue = AS_INTEGER(index);
+
+	if (argc == 1) {
+		/* Just try to call it */
+		EM_ASM({Module.krkb[$0]();}, returnValue);
+		return NONE_VAL();
+	} else if (argc == 2) {
+		KrkClass * type = AS_CLASS(krk_typeOf(1,&argv[1]));
+		krk_push(argv[1]);
+		KrkValue repr = krk_callSimple(OBJECT_VAL(type->_reprer), 1, 0);
+		krk_push(repr);
+		char * tmp = malloc(AS_STRING(repr)->length + AS_STRING(jsname)->length + 100);
+		sprintf(tmp, "(function (){try {\n%s(%s)\n} catch(e){Module.krkerr=e; return 1;} return 0;})()",AS_CSTRING(jsname), AS_CSTRING(repr));
+		int32_t result = emscripten_run_script_int(tmp);
+		free(tmp);
+		krk_pop();
+		if (result == 1) {
+			const char * s = krk_jsErr();
+			krk_runtimeError(vm.exceptions.valueError, "JS Error: %s", s);
+			free((char*)s);
+			return NONE_VAL();
+		}
+		return NONE_VAL();
+	} else {
+		krk_runtimeError(vm.exceptions.typeError, "Don't know how to call that.");
+		return NONE_VAL();
+	}
+}
+
+static KrkValue _jsexec(int argc, KrkValue argv[]) {
+	if (argc != 1 || !IS_STRING(argv[0])) {
+		krk_runtimeError(vm.exceptions.typeError, "must be string");
+		return NONE_VAL();
+	}
+	char * tmp = malloc(AS_STRING(argv[0])->length + 100);
+	sprintf(tmp, "(function (){try {\n%s\n} catch(e){Module.krkerr=e; return 1;} return 0;})()", AS_CSTRING(argv[0]));
+	int32_t result = emscripten_run_script_int(tmp);
+	free(tmp);
+	if (result == 1) {
+		const char * s = krk_jsErr();
+		krk_runtimeError(vm.exceptions.valueError, "JS Error: %s", s);
+		free((char*)s);
+		return NONE_VAL();
+	}
+	return NONE_VAL();
+}
 
 /**
  * This is built with NO_EXIT_RUNTIME, so when `main` returns none of the
@@ -34,6 +246,24 @@ int main() {
 	BUNDLED(dis);
 	BUNDLED(os);
 	BUNDLED(time);
+
+	emscripten_run_script("Module.krkb = [];");
+
+	KrkInstance * jsModule = krk_newInstance(vm.moduleClass);
+	krk_attachNamedObject(&vm.modules, "js", (KrkObj*)jsModule);
+	jsObjectClass = krk_newClass(krk_copyString("JSObject",8));
+	krk_attachNamedObject(&jsModule->fields, "JSObject", (KrkObj*)jsObjectClass);
+	jsObjectClass->base = vm.objectClass;
+	krk_tableAddAll(&vm.objectClass->methods, &jsObjectClass->methods);
+	krk_tableAddAll(&vm.objectClass->fields,  &jsObjectClass->fields);
+	/* Okay, how do we want to do this... */
+	krk_defineNative(&jsObjectClass->methods, ".__init__", _jsobject_init);
+	krk_defineNative(&jsObjectClass->methods, ".__getattr__", _jsobject_getattr);
+	krk_defineNative(&jsObjectClass->methods, ".__dir__", _jsobject_dir);
+	krk_defineNative(&jsObjectClass->methods, ".__call__", _jsobject_call);
+	krk_finalizeClass(jsObjectClass);
+
+	krk_defineNative(&jsModule->fields, "exec", _jsexec);
 
 	/* Set up the interpreter session */
 	krk_startModule("<module>");
